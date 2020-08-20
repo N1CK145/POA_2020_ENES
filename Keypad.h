efc9e8b81b08a684741ff3e081fbb8e418dfd04a
N1CK145 <nickas.gehlen@gmail.com>
2020-08-20 19:17:49 +0200

    Removed Files from repo

--- Keypad.h
+++
@@ -1,7 +0,0 @@
-#ifndef KEYPAD_H_INCLUDED
-#define KEYPAD_H_INCLUDED
-
-char readKeypadAndWriteToSegment();
-int isKeypadPressed();
-
-#endif // KEYPAD_H_INCLUDED
--- SevenSegmentDisplay.h
+++
@@ -1,9 +0,0 @@
-#ifndef SEVENSEGMENTDISPLAY_H_INCLUDED
-#define SEVENSEGMENTDISPLAY_H_INCLUDED
-
-void setSegment(int value);
-void segmentCountUp();
-void segmentCountDown();
-int getSegmentValue();
-
-#endif // SEVENSEGMENTDISPLAY_H_INCLUDED
